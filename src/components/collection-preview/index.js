50ce850fb9d3961cb3b9fefe6c3b028f4958ba19
lfbardi <luisfbardi@gmail.com>
2020-04-19 01:57:55 -0300

    collection preview

---
+++ src/components/collection-preview/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './styles.scss';
+
+import CollectionItem from '../collection-item';
+
+const CollectionPreview = ({ title, items }) => (
+    <div className="collection-preview">
+        <h1 className="title">{ title.toUpperCase() }</h1>
+
+        <div className="preview">
+            {
+                items
+                .filter((item, index) => index < 4)
+                .map( ({ id, ...props }) => (
+                    <CollectionItem key={id} {...props} />
+                ))
+            }
+        </div>
+    </div>
+);
+
+export default CollectionPreview;
