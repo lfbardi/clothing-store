6c4a035ce2e64443e01a22b524250ed2465d2319
lfbardi <luisfbardi@gmail.com>
2020-04-19 02:17:40 -0300

    renamed

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage';
 import ShopPage from './pages/shop';
 
+import Header from './components/header';
+
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
