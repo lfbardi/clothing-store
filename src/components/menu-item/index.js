8911d08cba55681ac5b37a31d10c58f32a889a3f
lfbardi <luisfbardi@gmail.com>
2020-04-19 01:58:09 -0300

    menu-item

--- src/components/menu-item/index.js
+++ src/components/menu-item/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import './styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+import { withRouter } from 'react-router-dom';
+
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
+  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div style={{
     backgroundImage: `url(${imageUrl})` 
   }} className="background-image" />
@@ -13,4 +15,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
