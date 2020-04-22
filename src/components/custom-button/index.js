4abe69837804f2224fd3906fa06d58ac9d50b7ef
lfbardi <luisfbardi@gmail.com>
2020-04-21 23:30:56 -0300

    google sign in fix

--- src/components/custom-button/index.js
+++ src/components/custom-button/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './styles.scss';
 
 const CustomButton = ({ children, isGoogleSignIn, ...props }) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} { ...props }>
+    <button type="button" className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} { ...props }>
         { children }
     </button>
 );
