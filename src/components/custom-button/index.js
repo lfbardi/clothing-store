7e415353a0f4b7a1910578aafb68bb1059ff9c62
lfbardi <luisfbardi@gmail.com>
2020-04-24 11:19:21 -0300

    inverted custom button

--- src/components/custom-button/index.js
+++ src/components/custom-button/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...props }) => (
-    <button type="button" className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} { ...props }>
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...props }) => (
+    <button type="button" className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} { ...props }>
         { children }
     </button>
 );
